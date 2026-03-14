27840042ecf594da49264799e75f53f4fd8b2d02
bageus <36338626+bageus@users.noreply.github.com>
2026-03-14 11:07:11 +0300

    Update state.js

--- js/state.js
+++ js/state.js
@@ -68,8 +68,7 @@ const gameState = {
 
   x2Timer: 0,
   uiUpdateFrame: 0,
-  renderFrame: 0,
-  
+
   centerOffsetX: 0,
   centerOffsetY: 0,
   spinCooldownReduction: 0,
@@ -86,10 +85,6 @@ const gameState = {
   lastSpinAlertRingDist: -999,
 
   spinComboCount: 0,
-  
-  renderQuality: 'high',
-  lowFpsStreak: 0,
-  highFpsStreak: 0,
 };
 
 const player = {
