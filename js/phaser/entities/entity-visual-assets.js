0e872b1eff068658244f5cdb56a7ded68f8f357d
bageus <36338626+bageus@users.noreply.github.com>
2026-04-11 13:01:13 +0200

    feat: add first wave of visual asset integrations

---
+++ js/phaser/entities/entity-visual-assets.js
@@ -0,0 +1,8 @@
+const VISUAL_UPGRADE_TEXTURES = Object.freeze({
+  shadow_contact_ellipse_01: 'img/new/shadow_contact_ellipse_01.png',
+  bonus_aura_soft_01: 'img/new/bonus_aura_soft_01.png',
+  coin_glint_star_01: 'img/new/coin_glint_star_01.png',
+  shock_ring_impact_01: 'img/new/shock_ring_impact_01.png',
+});
+
+export { VISUAL_UPGRADE_TEXTURES };
